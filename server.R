8b56f38517230d709b1049af4647bcf7f360d512
Huade <hh561@georgetown.edu>
2015-02-21 13:13:35 -0500

    rename title

--- server.R
+++ server.R
@@ -10,8 +10,12 @@ shinyServer(function(input, output) {
     if (length(input$name)==0) print("Please select at least one justice")
     
     else {
-      df_filtered <- SupCt %>% filter(Name %in% input$name)
-      graph_title  <- paste("Ideal Points", sep="")
+      df_filtered <- SupCt %>% 
+        filter(!is.na(Ideal.point)) %>%
+        filter(Name %in% input$name)
+      
+      j_names <- paste(input$name, collapse = ' and ')
+      graph_title  <- paste("Ideal Points for", j_names, sep="")
       
       
       ggideal_point <- ggplot(df_filtered,aes(x=Year, y=Ideal.point, by=Name, color=Name))+
@@ -20,8 +24,7 @@ shinyServer(function(input, output) {
         theme_bw()+
         labs(x = "Year")+
         labs(y = "Ideal Points")+
-        labs(title = graph_title)+
-        ylim(-2,2)
+        labs(title = graph_title)
       
       p <- plotly(username="Huade", key="t23brq57cr")
       res <- py$ggplotly(ggideal_point, kwargs=list(filename="Ideal Point", 
