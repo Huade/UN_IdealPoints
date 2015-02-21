b6206e2e55b24fbfce08816833eb7923fde4d666
Huade <hh561@georgetown.edu>
2015-02-21 13:03:45 -0500

    use plotly

--- server.R
+++ server.R
@@ -1,19 +1,39 @@
 library(shiny)
 library(dplyr)
 library(ggplot2)
+library(plotly)
+
 SupCt <- read.csv("SupCt.csv")
 shinyServer(function(input, output) {
-
-  output$trendPlot <- renderPlot({
-
-    df_filtered <- SupCt %>% filter(Name==input$name)
-    graph_title  <- paste("Ideal Points for ", input$name, sep="")
-    ggplot(df_filtered,aes(x=Year, y=Ideal.point))+
-      geom_point()+geom_smooth(se=F)+theme_bw()+
-      labs(x = "Year")+
-      labs(y = "Ideal Points")+
-      labs(title = graph_title)
-
+  
+  output$trendPlot <- renderUI({
+    if (length(input$name)==0) print("Please select at least one justice")
+    
+    else {
+      df_filtered <- SupCt %>% filter(Name %in% input$name)
+      graph_title  <- paste("Ideal Points", sep="")
+      
+      
+      ggideal_point <- ggplot(df_filtered,aes(x=Year, y=Ideal.point, by=Name, color=Name))+
+        geom_point()+
+        geom_line()+
+        theme_bw()+
+        labs(x = "Year")+
+        labs(y = "Ideal Points")+
+        labs(title = graph_title)+
+        ylim(-2,2)
+      
+      p <- plotly(username="Huade", key="t23brq57cr")
+      res <- py$ggplotly(ggideal_point, kwargs=list(filename="Ideal Point", 
+                                                    fileopt="overwrite", # Overwrite plot in Plotly's website
+                                                    auto_open=FALSE))
+      tags$iframe(src=res$response$url,
+                  frameBorder="0",  # Some aesthetics
+                  height=400,
+                  width=650)
+    }
+    
+    
   })
-
+  
 })
--- ui.R
+++ ui.R
@@ -8,19 +8,22 @@ shinyUI(fluidPage(
   # Sidebar with a slider input for number of bins
   
     sidebarPanel(
-      selectInput("name",
+      selectizeInput("name",
                   label = "Name",
                   choices = c("Black","Reed","Frankfurter","Jackson","Burton",
                               "Vinson","Clark","Minton","Douglas","Warren","Harlan","Brennan",
                               "Whittaker","Stewart","White","Goldberg","Fortas","Marshall",
                               "Burger","Blackmun","Powell","Rehnquist","Stevens","OConnor",
                               "Scalia","Kennedy","Souter","Thomas","Ginsburg","Breyer",
-                              "Roberts","Alito","Sotomayor","Kagen"))
+                              "Roberts","Alito","Sotomayor","Kagen"),
+                  multiple = T,
+                  options = list(maxItems = 3,
+                                 placeholder = 'Select a justice'))
       ),
 
     # Show a plot of the generated distribution
     mainPanel(
-      plotOutput("trendPlot")
+      htmlOutput("trendPlot")
     )
   )
 )
